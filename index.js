8364b1737d89daa6696379dd4c593289157ef4c5
louisonrails <237607115+louisonrails@users.noreply.github.com>
2025-10-22 08:49:10 -0300

    Add computer choice logic with getComputerChoice - Implemented getComputerChoice() to randomly return 'rock', 'paper', or 'scissors' - Enables the computer to make a random selection each round

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Rock.")
+console.log("Rock.");
+
+function getComputerChoice(){
+    number = Math.floor(Math.random() * 3) + 1;
+    if(number === 1){
+        return "rock";
+    }
+    if(number === 2){
+        return "paper";
+    }
+    return "scissors";
+}
+
